b92fe12ef8203067c1a78f972997ce308cb29be8
Shaheer <sm_shaheer@msn.com>
2022-05-26 18:05:30 +0500

    added free logo

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,7 +1,7 @@
-import logo from '../assets/images/logo.svg'
+import logo from '../assets/images/bbb.svg'
 
 const Logo = () => {
-  return <img src='' alt='logo' className='logo' />
+  return <img src={logo} alt='logo' className='logo' />
 }
 
 export default Logo
